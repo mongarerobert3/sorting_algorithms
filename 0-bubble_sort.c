3b58a02ff92f2ad50efec4ef2a1a6909672cbe58
mongarerobert3 <mongarerobert3@gmail.com>
2022-07-16 00:41:45 +0300

    task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,27 +7,28 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	int tmp, flag; /*flag is used to check if sorted */
-	size_t i, j;
+    /* tmp is for swap */
+    int tmp, flag; /*flag is used to check if sorted */
+    size_t i, j;
 
-	if (array == NULL || size == 0)
-		return;
+    if (array == NULL || size < 2) /* array does not need to be sorted if size is less than 2 */
+        return;
 
-	for (i = 0; i < size; i++)
-	{
+    for (i = 0; i < size; i++)
+    {
         flag = 0;
-		for (j = 0; j < size - 1; j++)
-		{
-			if (array[j] > array[j + 1])
-			{
-				tmp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = tmp;
-				print_array(array, size);
-                flag = 1;
-			}
-		}
+        for (j = 0; j < size - 1; j++) 
+        {
+            if (array[j] > array[j + 1]) /* compares integers of the array */
+            {
+                tmp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = tmp;
+                print_array(array, size);
+                flag = 1; /* tells swap has been made */
+            }
+        }
         if (flag == 0)
             return;
-	}
+    }
 }
