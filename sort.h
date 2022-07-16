00a441a5cae55bbdc6a2bd112059939e4a3c5d3d
dave motari <davemotari22@gmail.com>
2022-07-16 22:49:02 +0300

    Correction

--- sort.h
+++ sort.h
@@ -25,7 +25,7 @@ void bubble_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
-void swapNodes(listint_t **h, listint_t **node1, listint_t *node2);
+void _swap(listint_t **list, listint_t **node1, listint_t *node2);
 
 /*
 prototype of print function
