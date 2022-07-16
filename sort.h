2859a6efbdf83fddaaaa51858ae5383d35de862c
dave motari <davemotari22@gmail.com>
2022-07-16 22:18:16 +0300

    Header

--- sort.h
+++ sort.h
@@ -24,8 +24,12 @@ typedef struct listint_s
 void bubble_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void swapNodes(listint_t **h, listint_t **node1, listint_t *node2);
 
-/*prototype of print function*/
+/*
+prototype of print function
+*/
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 
